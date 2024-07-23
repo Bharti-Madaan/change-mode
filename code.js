40b7b317bc2262ec31f3069004e092809f8fc451
Bharti-Madaan <madaanb54@gmail.com>
2024-07-23 15:08:13 +0530

    addchangemode

---
+++ code.js
@@ -0,0 +1,21 @@
+let button = document.querySelector("#btn")
+let body = document.querySelector("body")
+
+let currmode = "light";
+
+button.addEventListener("click" , ()=>{
+   if(currmode === "light"){
+    currmode = "dark" 
+    body.classList.add("dark")
+    body.classList.remove("light")
+   }
+   else{
+    currmode = "light"
+    body.classList.add("light")
+    body.classList.remove("dark")
+    
+
+   }
+
+   console.log(currmode)
+})
